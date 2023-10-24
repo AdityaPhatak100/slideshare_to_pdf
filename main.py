bc1ba5586e70137d13c52062bd77f671cbf838a5
Aditya Phatak <adityaphatak216@gmail.com>
2023-10-24 16:12:03 +0530

    separate file for downloading logic

--- main.py
+++ main.py
@@ -1,98 +1,9 @@
-import math
-import requests
-import sys
 import os
-import time
-import shutil
 import argparse
-import urllib.request
-from bs4 import BeautifulSoup
-from PIL import Image
-from multiprocessing import Process
-
+from slidesharetopdf import SlideShareToPDF
 
 TEMP_IMAGES_PATH = "./TEMP_IMAGES_FOR_PPT"
 NUM_OF_PROCESSES = os.cpu_count() or 4
-
-
-def get_image_links_from_url(url: str) -> list[str]:
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    sources = soup.find_all("source")
-
-    image_links = [image.get("srcset").split(", ")[-1] for image in sources]
-
-    return image_links
-
-
-def create_temp_folder() -> None:
-    if not os.path.isdir(TEMP_IMAGES_PATH):
-        os.mkdir(TEMP_IMAGES_PATH)
-    else:
-        delete_temp_files()
-        create_temp_folder()
-
-
-def get_images(links: list[str], image_start_idx: int) -> None:
-    for itr, link in enumerate(links):
-        urllib.request.urlretrieve(
-            link.split(" ")[0], f"{TEMP_IMAGES_PATH}/{itr + image_start_idx}.jpg"
-        )
-
-
-def create_processes(links: list[str]) -> None:
-    links_per_process = math.ceil(len(links) / NUM_OF_PROCESSES)
-    processes = []
-
-    for i in range(1, NUM_OF_PROCESSES + 1):
-        if i != NUM_OF_PROCESSES:
-            process_args = [
-                links[links_per_process * (i - 1) : links_per_process * i],
-                links_per_process * (i - 1),
-            ]
-        else:
-            process_args = [
-                links[links_per_process * (i - 1) :],
-                links_per_process * (i - 1),
-            ]
-
-        processes.append(
-            Process(
-                target=get_images,
-                args=process_args,
-            )
-        )
-
-    run_processes(processes)
-
-
-def run_processes(processes: list[Process]) -> None:
-    for process in processes:
-        process.start()
-
-    for process in processes:
-        process.join()
-
-
-# append them into a single file
-def generate_pdf_from_images(url: str) -> None:
-    pdf_path = f"./{url.split('/')[-1].capitalize()}.pdf"
-
-    images = [
-        Image.open(f"{TEMP_IMAGES_PATH}/" + img) for img in os.listdir(TEMP_IMAGES_PATH)
-    ]
-
-    images[0].save(
-        pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
-    )
-
-    delete_temp_files()
-
-
-def delete_temp_files():
-    if os.path.isdir(TEMP_IMAGES_PATH):
-        shutil.rmtree(TEMP_IMAGES_PATH)
 
 
 if __name__ == "__main__":
@@ -104,10 +15,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m", help="Specify a text file path containing multiple slideshare links."
     )
+
     parser.add_argument("-l", help="Specify a single slideshare link.")
 
     args = parser.parse_args()
-
     if args.l and args.m:
         print(
             "Use either a link (-l) or a text file (-m). Cannot use both at the same time."
@@ -117,29 +28,20 @@ if __name__ == "__main__":
     if not args.l and not args.m:
         print(
             """Mention a slideshare url or a text file with multiple slideshare urls
-
-For example,
->>> python main.py -l "SLIDESHARE URL" 
-                OR
->>> python main.py -m "TEXT FILE PATH"
+                For example,
+                >>> python main.py -l "SLIDESHARE URL" 
+                                OR
+                >>> python main.py -m "TEXT FILE PATH"
               """
         )
-        
 
+    obj = SlideShareToPDF(TEMP_IMAGES_PATH, NUM_OF_PROCESSES)
+    
     if args.l:
         url = args.l
-
-        image_links = get_image_links_from_url(url)
-        create_temp_folder()
-        create_processes(image_links)
-        # st1 = time.perf_counter()
-        generate_pdf_from_images(url)
+        obj.download_pdf(url)
 
     if args.m:
         with open(args.m, "r") as file:
             for url in file.readlines():
-                url = url.strip()
-                image_links = get_image_links_from_url(url)
-                create_temp_folder()
-                create_processes(image_links)
-                generate_pdf_from_images(url)
+                obj.download_pdf(url)
---
+++ slidesharetopdf.py
@@ -0,0 +1,99 @@
+import os
+import math
+import shutil
+import urllib
+import requests
+from PIL import Image
+from bs4 import BeautifulSoup
+from multiprocessing import Process
+
+
+class SlideShareToPDF:
+    def __init__(self, temp_images_path, num_of_processes) -> None:
+        self.TEMP_IMAGES_PATH = temp_images_path
+        self.NUM_OF_PROCESSES = num_of_processes
+
+    def get_image_links_from_url(self, url: str) -> list[str]:
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        sources = soup.find_all("source")
+
+        image_links = [image.get("srcset").split(", ")[-1] for image in sources]
+
+        return image_links
+
+    def create_temp_folder(self) -> None:
+        if not os.path.isdir(self.TEMP_IMAGES_PATH):
+            os.mkdir(self.TEMP_IMAGES_PATH)
+        else:
+            self.delete_temp_files()
+            self.create_temp_folder()
+
+    def delete_temp_files(self):
+        if os.path.isdir(self.TEMP_IMAGES_PATH):
+            shutil.rmtree(self.TEMP_IMAGES_PATH)
+
+    def get_images(self, links: list[str], image_start_idx: int) -> None:
+        for itr, link in enumerate(links):
+            try:
+                urllib.request.urlretrieve(
+                    link.split(" ")[0],
+                    f"{self.TEMP_IMAGES_PATH}/{itr + image_start_idx}.jpg",
+                )
+            except urllib.error.HTTPError:
+                print("An Image was not found")
+
+    def create_processes(self, links: list[str]) -> None:
+        links_per_process = math.ceil(len(links) / self.NUM_OF_PROCESSES)
+        processes = []
+
+        for i in range(1, self.NUM_OF_PROCESSES + 1):
+            if i != self.NUM_OF_PROCESSES:
+                process_args = [
+                    links[links_per_process * (i - 1) : links_per_process * i],
+                    links_per_process * (i - 1),
+                ]
+            else:
+                process_args = [
+                    links[links_per_process * (i - 1) :],
+                    links_per_process * (i - 1),
+                ]
+
+            processes.append(
+                Process(
+                    target=self.get_images,
+                    args=process_args,
+                )
+            )
+
+        self.run_processes(processes)
+
+    def run_processes(self, processes: list[Process]) -> None:
+        for process in processes:
+            process.start()
+
+        for process in processes:
+            process.join()
+
+    def generate_pdf_from_images(self, url: str) -> None:
+        pdf_path = f"./{url.split('/')[-1].capitalize()}.pdf"
+
+        images = [
+            Image.open(f"{self.TEMP_IMAGES_PATH}/" + img)
+            for img in os.listdir(self.TEMP_IMAGES_PATH)
+        ]
+
+        images[0].save(
+            pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
+        )
+
+        self.delete_temp_files()
+
+    def download_pdf(self, url: str) -> None:
+        url = url.strip()
+        
+        image_links = self.get_image_links_from_url(url)
+        self.create_temp_folder()
+        self.create_processes(image_links)
+        self.generate_pdf_from_images(url)
