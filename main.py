3f46c111f14c3e0a61524ca24eff6c65d800f99f
Aditya Phatak <adityaphatak216@gmail.com>
2023-10-25 19:48:33 +0530

    renamed images_path

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from slidesharetopdf import SlideShareToPDF
 
-TEMP_IMAGES_PATH = "./TEMP_IMAGES_FOR_PPT"
+TEMP_IMAGES_PATH = "./TEMP_IMAGES_FOR_PDF"
 NUM_OF_PROCESSES = os.cpu_count() or 4
 
 
